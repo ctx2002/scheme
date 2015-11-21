29369a5ca2c8d1989f07d32bda5b3729d0726897
anru <ctx2002@gmail.com>
2015-11-21 21:46:15 +1300

    identity and self_apply funciton

--- lambda.rkt
+++ lambda.rkt
@@ -19,8 +19,29 @@
     (lambda (b)
       (lambda (c)
         (b ((a b) c))))))
+
+(define identity 
+  lambda (x)
+  x)
+
+(define self_apply
+  (lambda (s)
+    (s s)))
+
+(define apply
+  (lambda (func)
+    (lambda (arg)
+      (func arg))))
+
+; build an identity function base on self_apply function
+(define identity2
+  (lambda (x)
+    ((apply identity)
+     x)))
+
+
 ;now we need to resolve this lzero
 ((lzero  add1) 0)
 ((lone  add1) 0)
 ((ltwo  add1) 0)
-((lzero lsuccessor) 5)
+((lone lsuccessor) lone)
